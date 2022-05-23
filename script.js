d785f22947503471d58ca83a4862d3251902498c
Avery Yang <averyyang2674@gmail.com>
2022-05-22 21:22:17 -0500

    add remove active classes function

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const panels = document.querySelectorAll('.panel');
 
 // forEach = higher-order method that accepts functions as arguments
 // purpose: add eventListener to each panel that expands when clicked
-panels.forEach((panel) => {
+panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        console.log(123);
+        removeActiveClasses();
+        panel.classList.add('active');
     })
 });
+
+// purpose: removes active classes so they collapse when clicked
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+}
